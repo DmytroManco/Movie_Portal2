d2f962a93f7072ef29f6a89bdbe9a68438341c63
Dmytro Putrin <Dmytro_Putrin@epam.com>
2017-10-12 10:38:19 +0300

    HW: refactor movies.service.ts

--- src/app/core/services/movies.service.ts
+++ src/app/core/services/movies.service.ts
@@ -2,46 +2,55 @@ import { Injectable } from '@angular/core';
 import { Http, Headers, Response } from '@angular/http';
 import { Observable } from 'rxjs';
 import'rxjs/operator/map';
+import { Movie } from '../../features';
 import { Router } from '@angular/router';
-const url: string = 'http://localhost:8000/movies';
 
 @Injectable()
 export class MoviesService {
+  private url: string = 'http://localhost:8000/movies';
 
   constructor(private http: Http, private router: Router) {}
 
-  public fetchData() {
-    return this.http.get(url)
-      .map((response: Response) => response.json())
-      .catch((error: Response) => Observable.throw(error.json().error || 'Server error'));
+  public fetchData(): Observable<Movie[]> {
+    return this.http.get(this.url)
+      .map((response: Response) => this.returnData(response))
+      .catch((error: Response) => this.returnError(error));
   }
 
-  public updateMovie(movie) {
+  public updateMovie(movie: Movie) {
     let headers = new Headers();
     headers.append('Content-Type', 'application/json');
 
-    return this.http.put(`${url}/${movie.id}`, JSON.stringify(movie), {headers})
-      .map((response: Response) => response.json()).subscribe();
+    return this.http.put(`${this.url}/${movie.id}`, JSON.stringify(movie), {headers})
+      .map((response: Response) => this.returnData(response)).subscribe();
   }
 
-  public sortMovies(value: string) {
-    return this.http.get(`${url}?_sort=${value}&_order=desc`)
-      .map((response: Response) => response.json())
-      .catch((error: Response) => Observable.throw(error.json().error || 'Server error'));
+  public sortMovies(value: string): Observable<Movie[]>  {
+    return this.http.get(`${this.url}?_sort=${value}&_order=desc`)
+      .map((response: Response) => this.returnData(response))
+      .catch((error: Response) => this.returnError(error));
   }
 
-  public filterMovie(filter: string) {
-    return this.http.get(`${url}?q=${filter}`)
-      .map((response: Response) => response.json())
-      .catch((error: Response) => Observable.throw(error.json().error || 'Server error'));
+  public filterMovie(filter: string): Observable<Movie[]>  {
+    return this.http.get(`${this.url}?q=${filter}`)
+      .map((response: Response) => this.returnData(response))
+      .catch((error: Response) => this.returnError(error));
   }
 
-  public getMovie(id: number) {
-    return this.http.get(`${url}/${id}`)
-      .map((response: Response) => response.json())
+  public getMovie(id: number): Observable<Movie> {
+    return this.http.get(`${this.url}/${id}`)
+      .map((response: Response) => this.returnData(response))
       .catch((error: Response) => {
         this.router.navigate(['**']);
-        return Observable.throw(error.json().error || 'Server error');
+        return this.returnError(error);
       });
+  }
+
+  private returnData(response): Observable<Movie> {
+    return response.json();
+  }
+
+  private returnError(error): Observable<any> {
+    return Observable.throw(error.json().error || 'Server error');
   }
 }
--- src/app/features/movies/index.ts
+++ src/app/features/movies/index.ts
@@ -3,3 +3,4 @@ export * from './movie_item';
 export * from './movies.component';
 export * from './movie_detail';
 export * from './movies.routes';
+export * from './movie.model';
